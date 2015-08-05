573e57b8683b5eb38d5b0929d6879c644be4cc72
unknown <Daniel.Copaciu@CJ002237PC.esp.local>
2015-08-05 10:24:11 +0300

    Authentication feature - start

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -4,13 +4,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/")
 public class HelloController {
-	@RequestMapping(method = RequestMethod.GET)
+
+
+	@RequestMapping(value = {"/", "/welcome**"}, method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello world!");
 		return "hello";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login (@RequestParam(value = "error", required = false) String error,
+						 ModelMap model){
+
+		if(error != null){
+			model.addAttribute("error", "Invalid username and password");
+		}
+		return "hello";
+	}
+
+	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
+	public String printAdmin(ModelMap model){
+		return "admin";
 	}
 }
--- target/OnlineStore/WEB-INF/pages/hello.jsp
+++ target/OnlineStore/WEB-INF/pages/hello.jsp
@@ -1,5 +1,27 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <body>
-	<h1>${message}</h1>
+<h1>Welcome</h1>
+
+<form name="loginForm" action=/j_spring_security_check method="POST">
+    <table align="center">
+        <tr>
+            <td>User:</td>
+            <td><input type="text" name="user"></td>
+        </tr>
+        <tr>
+            <td>Password:</td>
+            <td><input type="password" name="password"></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td align="right"><input type="submit" name="submit" value="Log In!"></td>
+        </tr>
+    </table>
+    <input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}">
+</form>
+<c:if test="${not empty error}">
+    <p>${error}</p>
+</c:if>
 </body>
 </html>
